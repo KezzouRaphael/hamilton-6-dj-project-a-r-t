167aae9ccc282fc3f27f17daa8964e4ed5bb53a9
Raphael Kezzou <Raphael.Kezzou@live.fr>
2022-03-08 14:15:17 +0100

    still needs styling

--- js/script.js
+++ js/script.js
@@ -11,4 +11,14 @@ function carrouselclicked(e){
     links.removeAttribute("id");
   }
   e.target.setAttribute("id","active-list");
+  let hreflink = e.target.getAttribute("href");
+  console.log(hreflink);
+  let node = document.querySelector(hreflink);
+  let content = document.querySelectorAll(".scheduler-content-month");
+  for(el of content)
+  {
+    el.removeAttribute("class");
+    el.setAttribute("class","scheduler-content-month");
+  }
+  node.setAttribute("class","scheduler-content-month active-content");
 }
